ca1469d53ac94d6df61cc26d2d1f740a7e800a3f
unknown <yinwei>
2018-03-08 15:52:44 +0800

    swipe and gird

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 // 入口文件
-console.log('OK');
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+import {Header, Swipe, SwipeItem} from 'mint-ui'
+Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+// 2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+Vue.use(VueResource)
+import app from './App.vue'
+import router from './router.js'
+
+var vm=new Vue({
+    el:"#app",
+    render: c => c(app),
+    router
+})
--- src/router.js
+++ src/router.js
@@ -1,10 +1,20 @@
 import VueRouter from 'vue-router'
 
+import Home from './components/HomeContainer.vue'
+import ShopContainer from './components/ShopContainer.vue'
+import MemberContainer from './components/MemberContainer.vue'
+import SearchContainer from './components/SearchContainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-  ]
+      {path:'/',redirect:"/home"},
+      {path:'/home',component:Home},
+      {path:'/shopcar',component:ShopContainer},
+      {path:'/member',component:MemberContainer},
+      {path:'/search',component:SearchContainer}
+  ],
+  linkActiveClass:'mui-active'
 })
 
 // 把路由对象暴露出去
